901d00a2d1c5750694145acd916de81bc9051e53
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-05-15 14:46:59 +0200

    Add styling

--- src/components/LightDarkMode.js
+++ src/components/LightDarkMode.js
@@ -11,7 +11,7 @@ export default function LightDarkMode() {
   console.log(theme);
 
   return (
-    <div className="light-dark-mode">
+    <div className="light-dark-mode" data-theme={theme}>
       <div className="container">
         <p>Hello World!</p>
         <button onClick={handleToggleTheme}>Change Theme</button>
